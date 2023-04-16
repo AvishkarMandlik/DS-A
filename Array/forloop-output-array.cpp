b5843988196c292ff2f5451125f240c4758c6653
AvishkarMandlik <mandlikavi121@gmail.com>
2023-04-16 20:03:35 +0530

    forloop output array added

---
+++ Array/forloop-output-array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+  
+int main() {
+  int mks[6] = {1, 2, 3, 4, 23, 23};
+  
+  for(int i=0; i<5; i++)
+  {
+  	cout<<"Marks :-"<<mks[i]<<endl;
+  }
+
+    return 0;
+}
+
