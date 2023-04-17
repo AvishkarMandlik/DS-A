8b05aec7491e2d445c6904847cf53a0e56d42fa1
AvishkarMandlik <mandlikavi121@gmail.com>
2023-04-17 21:26:49 +0530

    for loop input otput program added

---
+++ Array/forloop-inputandoutput-array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+  
+int main() {
+  int mks[6] ;
+  
+  for(int i=0; i<5; i++)
+  {
+  	cout<<"Enter value of index :-";
+        cin>>mks[i];
+  }
+        
+ for(int i=0; i<5; i++)
+  {
+        cout<<mks[i]<<endl;
+  }
+
+    return 0;
+}
+
