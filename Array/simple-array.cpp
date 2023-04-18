53a2c823f7878131de7755960cc8c7783f6c23be
AvishkarMandlik <mandlikavi121@gmail.com>
2023-04-18 19:40:46 +0530

    simple array added

---
+++ Array/simple-array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int ages[5];
+   ages[0] = 2;
+     ages[1] = 32;
+      ages[2] = 56;
+       ages[3] = 4;
+         ages[4] = 22;
+        cout<<ages[0]<<endl;
+        cout<<ages[1]<<endl;
+        cout<<ages[2]<<endl;
+        cout<<ages[3]<<endl;
+        cout<<ages[4]<<endl;
+  return 0;
+}
+
