8aa00d6e38966e1d3ef9015fc39e569ce19828e9
AvishkarMandlik <mandlikavi121@gmail.com>
2023-04-25 19:45:27 +0530

    String folder added

---
+++ String/simple-String.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+
+    string name;
+
+    cout<<"Enter your name:";
+    cin>>name;
+
+    cout<<"Your Name :-"<<name;
+
+    return 0;
+}
