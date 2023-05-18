5288036d592d65bdc640678e06a1110a1985d15d
AvishkarMandlik <mandlikavi121@gmail.com>
2023-05-18 16:43:00 +0530

    hare and tortoise to find link list mid element

---
+++ Linklist/hare-tortoise.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class Node
+{
+    public:
+    int data;
+    Node *next;
+};
+
+int main()
+{
+    Node *p = new Node();
+    p->data = 2;
+    Node *q = new Node();
+    q->data = 4;
+    Node *r = new Node();
+    r->data = 6;
+    Node *s = new Node();
+    s->data = 8;
+    Node *t = new Node();
+    t->data = 10;
+    
+    p->next = q;
+    q->next = r;
+    r->next = s;
+    s->next = t;
+    t->next = NULL;
+    
+    Node *slow = p;
+    Node *fast = q;
+    
+    while(fast!=NULL && fast->next !=NULL)
+    {
+        fast = fast->next;
+        fast = fast->next;
+        slow = slow->next;
+    }
+    
+    cout<<"Mid Element :- "<<slow->data<<endl;
+    
+    return 0;
+}
+
